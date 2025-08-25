63341862b17b7322f76e67fc08316671b69bf4f1
Anobhav <singhanobhav@gmail.com>
2025-08-25 07:53:44 +0530

    comditionals in python

---
+++ conditionals.py
@@ -0,0 +1,23 @@
+x=int(input('enter your marks \n'))
+
+if x>90:
+    print(" Grade : A+")
+
+
+elif x>80:
+    print(" Grade : A")
+
+elif x>70:
+    print("Grade : B+")
+
+elif x>60:
+    print("Grade : B")
+
+elif x>50:
+    print("Grade : C")
+
+elif x>40:
+    print("Grade : D")
+
+else :
+    print(" Fail ")
