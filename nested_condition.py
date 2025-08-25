cc6ef36dc027e3ac3561ca3721937bba4e7dd0a6
Anobhav <singhanobhav@gmail.com>
2025-08-25 08:31:08 +0530

    changes

---
+++ nested_condition.py
@@ -0,0 +1,14 @@
+x= int(input("enter a number"))
+
+if x>20:
+    print("number greater than 20 calculating : ")
+
+    if x%2==0 :
+        print("number is greater than 20 and it is even")
+
+    else :
+        print("number is greatet than 20 but it is odd")
+
+else :
+    print("number is not greater than 20 so not calculating")
+
---
+++ switch_statements.py
@@ -0,0 +1,29 @@
+# match and case 
+
+day_num=5
+
+match day_num :
+    case 1:
+        print("Monday")
+
+    case 2:
+        print("Tuesday")
+
+    case 3:
+        print("Wednesday")
+
+    case 4:
+        print("Thusday")
+
+    case 5:
+        print("Friday")
+
+    case 6:
+        print("Saturday")
+
+    case 7:
+        print("Sunday")
+
+    case default :
+        print("invalid input")
+
